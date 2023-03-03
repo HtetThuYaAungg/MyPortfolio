261dd2606975e085015aed47ff9602179adc2487
Htet Thura Aung <htetthuraaung@abank.com.mm>
2023-03-03 16:15:21 +0630

    Initial

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
         <div class="home-hero__social">
           <a href="#" class="home-hero__social-icon-link">
             <img
-              src="pp1.jpg"
+              src="/github-ico.png"
               alt="icon"
               class="home-hero__social-icon"
             />
