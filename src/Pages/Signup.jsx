0785dd0632e30ed791212c5a9e1122e32140ae2c
Htet Thura Aung <htetthuraaung@abank.com.mm>
2023-03-07 20:52:51 +0630

    Initial

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -52,58 +52,58 @@ const Signup = () => {
 
   return (
     <div className='Login'>
-      <div className="home-hero hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <h1 className="heading-primary">Sign Up For Admin </h1>
-          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl">
-            <form className="card-body" >
-              <div className="form-control justify-center">
-                <label className="label">
-                  <span className="label-text text-white">E-Mail</span>
-                </label>
-
-                <input
-                  label="Email"
-                  type="email"
-                  variant="outlined"
-                  name="email"
-                  value={form.email}
-                  onInput={onFormInputChange}
-
-                  placeholder="Admin E-mail Address"
-                  className="input input-bordered bg-white text-green-900"
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text text-white">Password</span>
-                </label>
-                <input
-                  label="Password"
-                  type="password"
-                  variant="outlined"
-                  name="password"
-                  value={form.password}
-                  onChange={onFormInputChange}
-                  placeholder="password" className="input input-bordered bg-white text-green-900"
-                />
-              </div>
-              <div className="form-control mt-6 flex-row">
-                <button variant={"contained"} className={`btn btn-primary ${isLoading && "btn-disabled"}  mr-2`} onClick={onSubmit}>
-                  {isLoading ? <ImSpinner2 className='animate-spin' /> : "SignUp"}
+    <div className="home-hero hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <h1 className="heading-primary">SignUp For Admin </h1>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl">
+          <form className="card-body" >
+            <div className="form-control justify-center">
+              <label className="label">
+                <span className="label-text text-white">E-Mail</span>
+              </label>
+              <input
+                label="Email"
+                type="email"
+                variant="outlined"
+                name="email"
+                autoComplete="email"
+                value={form.email}
+                onChange={onFormInputChange}
+                placeholder="Admin E-mail Address"
+                className="input input-bordered bg-white text-green-900"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text text-white">Password</span>
+              </label>
+              <input
+                label="Password"
+                type="password"
+                variant="outlined"
+                name="password"
+                autoComplete="current-password"
+                value={form.password}
+                onChange={onFormInputChange}
+                placeholder="password" className="input input-bordered bg-white text-green-900"
+              />
+            </div>
+            <div className="form-control mt-6 flex-row">
+              <button variant={"contained"} className={`btn btn-primary ${isLoading && "btn-disabled"} mr-2`} onClick={onSubmit}>
+                {isLoading ? <ImSpinner2 className='animate-spin' /> : "SignUp"}
+              </button>
+              <Link to="/">
+                <button className="btn btn-primary">
+                  Cancel
                 </button>
-                <Link to="/add">
-                  <button className="btn btn-primary mr-2">
-                    Cancel
-                  </button>
-                </Link>
-        
-              </div>
-            </form>
-          </div>
+              </Link>
+
+            </div>
+          </form>
         </div>
       </div>
     </div>
+  </div>
   )
 }
 
