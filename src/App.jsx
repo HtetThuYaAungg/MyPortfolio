52f4245702e0ed19142a4acea60790be7bb6e5b8
Htet Thura Aung <htetthuraaung@abank.com.mm>
2023-03-08 12:46:13 +0630

    Initial

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
   return (
     <div className='App'>
       
-      {width < 365 ? <UnSupported/> :
+      {width < 360 ? <UnSupported/> :
          <Start /> 
       }
      
