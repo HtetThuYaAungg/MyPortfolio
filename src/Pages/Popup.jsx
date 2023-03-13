cafc2d10de607d9eeca822235e47deaac500c10b
Htet Thura Aung <htetthuraaung@abank.com.mm>
2023-03-13 11:58:36 +0630

    Initial

--- src/Pages/Popup.jsx
+++ src/Pages/Popup.jsx
@@ -14,7 +14,9 @@ import RadialPopup from "./RadialPopup";
 import { TabContext, TabList, TabPanel } from "@mui/lab";
 import { Tab } from "@mui/material";
 import StepList from "../components/StepList";
-
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { green, orange } from "@mui/material/colors";
+import Training from "../components/Training";
 
 const drawerBleeding = 56;
 
@@ -40,6 +42,14 @@ const Puller = styled(Box)(({ theme }) => ({
   top: 8,
   left: "calc(50% - 15px)",
 }));
+
+const outerTheme = createTheme({
+  palette: {
+    primary: {
+      main: green[900],
+    },
+  },
+});
 
 const Popup = ({ window, open, setOpen, toggleDrawer }) => {
   const today = new Date();
@@ -94,19 +104,19 @@ const Popup = ({ window, open, setOpen, toggleDrawer }) => {
               className=" justify-center flex space-x-7"
               sx={{ p: 2, color: "text.secondary" }}
             >
-             
-                <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+              <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                <ThemeProvider theme={outerTheme}>
                   <TabList
                     onChange={handleChange}
                     aria-label="lab API tabs example"
-                    sx={{color:'white'}}
+                    sx={{ color: "white" }}
                   >
-                    <Tab   label="Programming" value="1" />
+                    <Tab label="Programming" value="1" />
                     <Tab label="Languages" value="2" />
                     <Tab label="Training" value="3" />
                   </TabList>
-                </Box>
-             
+                </ThemeProvider>
+              </Box>
             </Typography>
 
             <StyledBox
@@ -115,8 +125,8 @@ const Popup = ({ window, open, setOpen, toggleDrawer }) => {
                 height: {
                   xs: "calc(100vh - 300px - 110px)",
                   sm: "calc(100vh - 200px - 10px)",
-                  md:"calc(100vh - 200px - 100px)",
-                } ,
+                  md: "calc(100vh - 200px - 100px)",
+                },
                 overflow: "auto",
               }}
             >
@@ -124,14 +134,40 @@ const Popup = ({ window, open, setOpen, toggleDrawer }) => {
                 <RadialPopup />
               </TabPanel>
               <TabPanel value="2">
-               <p className=" mb-6 px-5 font-bold text-green-900 sm:px-20 lg:px-36">Burmese</p>
-                <StepList activeStep={2} level1={'Beginner'} level2={'Junior'} level3={ 'Intermediate' } level4={ 'Pro'} />
-                <p className=" mb-6 px-5 mt-10 font-bold text-green-900 sm:px-20 lg:px-36">English</p>
-                <StepList  activeStep={1} level1={'Beginner'} level2={'Junior'} level3={ 'Intermediate' } level4={ 'Pro'}/>
-                <p className=" mb-6 px-5 mt-10 font-bold text-green-900 sm:px-20 lg:px-36">Japanese</p>
-                <StepList activeStep={1} level1={'N5'} level2={'N4'} level3={'N3'} level4={'N2'} />
+                <p className=" mb-6 px-5 font-bold text-green-900 sm:px-20 lg:px-36">
+                  Burmese
+                </p>
+                <StepList
+                  activeStep={2}
+                  level1={"Beginner"}
+                  level2={"Junior"}
+                  level3={"Intermediate"}
+                  level4={"Pro"}
+                />
+                <p className=" mb-6 px-5 mt-10 font-bold text-green-900 sm:px-20 lg:px-36">
+                  English
+                </p>
+                <StepList
+                  activeStep={1}
+                  level1={"Beginner"}
+                  level2={"Junior"}
+                  level3={"Intermediate"}
+                  level4={"Pro"}
+                />
+                <p className=" mb-6 px-5 mt-10 font-bold text-green-900 sm:px-20 lg:px-36">
+                  Japanese
+                </p>
+                <StepList
+                  activeStep={1}
+                  level1={"N5"}
+                  level2={"N4"}
+                  level3={"N3"}
+                  level4={"N2"}
+                />
               </TabPanel>
-              <TabPanel value="3">Item Three</TabPanel>
+              <TabPanel value="3">
+                <Training/>
+              </TabPanel>
             </StyledBox>
           </TabContext>
           {/* <Typography className=" justify-center flex space-x-7" sx={{ p: 2, color: 'text.secondary' }}><p className='text-green-900 hover:text-green-700'>Skills and Tools &copy; {today.getFullYear()}</p></Typography> */}
